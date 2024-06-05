717d9f66e409d835e6d8284f66051133d4747ea2
innovationsquared <pettiscd@appstate.edu>
2024-06-05 09:42:06 -0400

    fire nvim config

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -51,5 +51,15 @@ return {
     -- Footer
     dashboard.section.footer.val = { "Cal's setup :)" }
     alpha.setup(dashboard.config)
+    vim.api.nvim_create_autocmd('User', {
+      once = true,
+      pattern = 'LazyVimStarted',
+      callback = function()
+        local stats = require('lazy').stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = '󰈸 Loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms'
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
   end,
 }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,9 +1,1 @@
-return {
-  'edluffy/hologram.nvim',
-  config = function()
-    -- Call the setup function for hologram.nvim if required
-    require('hologram').setup {
-      auto_display = true, -- Automatically display images
-    }
-  end,
-}
+return {}
